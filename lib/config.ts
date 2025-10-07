9e59fa5fdb32ffa97c8cc1bb7f06c701ee4a571c
amro22397 <amroalmutasim22@gmail.com>
2025-10-07 10:32:14 +0400

    many changes 7-oct-2025 1032 imagekit api 4

--- lib/config.ts
+++ lib/config.ts
@@ -3,9 +3,9 @@ const config = {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
     prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
-      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
-      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+      publicKey: process.env.NEXT_PUPLIC_IMAGEKIT_PUPLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUPLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.NEXT_PUPLIC_IMAGEKIT_PRIVATE_KEY!,
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
