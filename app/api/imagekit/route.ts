38b9b6926522c9e5fbdcfde512376b94405f56d0
amro22397 <amroalmutasim22@gmail.com>
2025-10-07 10:28:33 +0400

    many changes 7-oct-2025 1028 imagekit api 3

--- app/api/imagekit/route.ts
+++ app/api/imagekit/route.ts
@@ -1,23 +1,15 @@
-import config from "@/lib/config";
 import ImageKit from "imagekit";
+import config from "@/lib/config";
 import { NextResponse } from "next/server";
 
-
 const {
-    env: {
-        imagekit: {
-            publicKey, privateKey, urlEndpoint
-        }
-    }
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
 } = config;
-
 
 const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
 
-
 export async function GET() {
- 
-    
-    return NextResponse.json(imagekit.getAuthenticationParameters());
-    
+  return NextResponse.json(imagekit.getAuthenticationParameters());
 }
